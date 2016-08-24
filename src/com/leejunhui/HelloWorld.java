9702e35a0766a344c9484db30b78d0e2481b64ac
LeeJunhui <lijunhui_ios@qq.com>
2016-08-24 15:16:26 +0800

    Initial commit

---
+++ src/com/leejunhui/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.leejunhui;
+
+/**
+ * Created by leejunhui on 8/24/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
