578468d5c6dad992ed0f02cb1cd02f4b28333b3a
LeeJunhui <84416097@qq.com>
2016-08-24 15:27:34 +0800

    commit the file

--- src/com/leejunhui/HelloWorld.java
+++ src/com/leejunhui/HelloWorld.java
@@ -5,6 +5,15 @@ package com.leejunhui;
  */
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("Hello World!");
+
+//        System.out.print("Hello World!");
+        // Java 学习之路
+        String name = "leejunhui";
+        Integer age = 22;
+
+        System.out.print(name);
+        System.out.print("\n");
+        System.out.print(age);
+
     }
 }
