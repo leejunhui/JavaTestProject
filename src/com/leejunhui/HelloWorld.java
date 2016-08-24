bbf14373431e781aab29982612015f19d9cebb71
LeeJunhui <84416097@qq.com>
2016-08-24 16:17:44 +0800

    Modify the FIle Template

--- src/com/leejunhui/HelloWorld.java
+++ src/com/leejunhui/HelloWorld.java
@@ -1,19 +1,14 @@
 package com.leejunhui;
 
 /**
- * Created by leejunhui on 8/24/16.
- */
+ * Hello World
+ *
+ * @author leejunhui
+ * @create 2016 08 24 4:15 PM
+ **/
+
 public class HelloWorld {
     public static void main(String[] args){
-
-//        System.out.print("Hello World!");
-        // Java 学习之路
-        String name = "leejunhui";
-        Integer age = 22;
-
-        System.out.print(name);
-        System.out.print("\n");
-        System.out.print(age);
-
+        System.out.print("Hello World!");
     }
 }
